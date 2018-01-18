245b1eccc1548cd562919bca695972682ffd0c70
rvaldenegrof <r.valdenegrof@gmail.com>
2018-01-18 13:35:07 -0300

    primer commit

---
+++ parts/header.php
@@ -0,0 +1,31 @@
+<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+		<div id="fh5co-header">
+			<header id="fh5co-header-section">
+				<div class="container">
+					<div class="nav-header">
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+						<h1 id="fh5co-logo"><a href="index.html">Han<span>Moo</span></a></h1>
+						<!-- START #fh5co-menu-wrap -->
+						<nav id="fh5co-menu-wrap" role="navigation">
+							<ul class="sf-menu" id="fh5co-primary-menu">
+								<li class="active">
+									<a href="index.php">Inicio</a>
+								</li>
+								<li>
+									<a href="taekwondo.php" class="fh5co-sub-ddown">Taekwondo</a>
+									 <ul class="fh5co-sub-menu">
+									 	<li><a href="left-sidebar.html">Historia Club deportivo Hanmoo</a></li>
+									 </ul>
+								</li>
+								<li><a href="noticias.php">Noticias</a></li>
+								<li><a href="entrenadores.php">Entrenadores</a></li>
+								<li><a href="inscripcion.php">Inscripción</a></li>
+								<li><a href="contacto.php">Contacto</a></li>
+								<li><a href="quienes.php">Quiénes Somos</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>		
+		</div>
